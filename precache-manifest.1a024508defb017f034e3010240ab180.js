b96c59270da6e391233428831ca035905198ea76
paulwebdevgit <paulkonev94@gmail.com>
2020-11-02 19:31:13 +0100

    Updates

---
+++ precache-manifest.1a024508defb017f034e3010240ab180.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c74bce91fbcab83d43bf00d664420e",
+    "url": "/calcreact/index.html"
+  },
+  {
+    "revision": "c8cd699feb056392262f",
+    "url": "/calcreact/static/css/main.c7ebeecd.chunk.css"
+  },
+  {
+    "revision": "bc97a438cdacb6cba764",
+    "url": "/calcreact/static/js/2.4f76cc04.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/calcreact/static/js/2.4f76cc04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8cd699feb056392262f",
+    "url": "/calcreact/static/js/main.11cb0af5.chunk.js"
+  },
+  {
+    "revision": "0ee5f7e00d1c705edb09",
+    "url": "/calcreact/static/js/runtime-main.670f007e.js"
+  }
+]);
