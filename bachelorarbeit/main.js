26396f142c8e4fce5baf7d63c0025c3403cc9f86
paulwebdevgit <paulkonev94@gmail.com>
2021-02-24 22:44:08 +0100

    scope

--- bachelorarbeit/main.js
+++ bachelorarbeit/main.js
@@ -28,7 +28,7 @@ if ('serviceWorker' in navigator) {
     window.addEventListener("load", () => {
 
         navigator.serviceWorker
-            .register('/paulwebdevgit.github.io/bachelorarbeit/sw.js', { scope: '/paulwebdevgit.github.io/bachelorarbeit/' })
+            .register('/bachelorarbeit/sw.js', { scope: '/bachelorarbeit/' })
             .then((reg) => {
                 // Registrierung erfolgreich
                 console.log('Registrierung erfolgreich. Scope ist ' + reg.scope);
