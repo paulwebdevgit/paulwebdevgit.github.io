6169adeef3d81e9569bb79249f9a70a3edb28ca3
paulwebdevgit <paulkonev94@gmail.com>
2021-05-01 16:12:19 +0200

    stop/start button

--- gameoflife/life.js
+++ gameoflife/life.js
@@ -13,120 +13,136 @@ button.addEventListener('click', go);
 
 
 function go() {
-  let resolution = 10;
-  // let spalte = +w.value;
-  // let reihe = +h.value;
-  // console.log(reihe, spalte);
-  canvas.width = +w.value;
-  canvas.height = +h.value;
-  spalte = canvas.width / resolution;
-  reihe = canvas.height / resolution;
-  
-  // requestAnimationFrame(anim);
+    let resolution = 10;
+    // let spalte = +w.value;
+    // let reihe = +h.value;
+    // console.log(reihe, spalte);
+    canvas.width = +w.value;
+    canvas.height = +h.value;
+    spalte = canvas.width / resolution;
+    reihe = canvas.height / resolution;
 
-  function initial2DArray(spalte, reihe) {
-    let arr = new Array(spalte);
-    for (let i = 0; i < arr.length; i++) {
-      arr[i] = new Array(reihe);
-    }
-    return arr;
-  }
-  function initialize() {
-    grid = initial2DArray(spalte, reihe);
-    for (let i = 0; i < spalte; i++) {
-      for (let j = 0; j < reihe; j++) {
-        grid[i][j] = Math.floor(Math.random() * 2);
-      }
-    }
-    // console.table(grid);
-  }
-  initialize();
+    // requestAnimationFrame(anim);
 
-  
-  function anim (){
-    try {
-      requestAnimationFrame(anim);
-      render();
-    } catch (err) {}
-   
-  }
-  anim();
-
-  function stop(){
-      anim = null;
-  }
-
-  button2.addEventListener('click', stop);
-  
-  
-  function render() {
-    for (let i = 0; i < spalte; i++) {
-      for (let j = 0; j < reihe; j++) {
-        let x = i * resolution;
-        let y = j * resolution;
-        // console.log(grid[i][j]);
-        if (grid[i][j] == 1) {
-          ctx.beginPath();
-          ctx.rect(x, y, resolution, resolution);
-          ctx.fillStyle = 'black';
-          ctx.fill();
-          ctx.strokeStyle = 'white';
-          ctx.stroke();
-        }else{
-          ctx.beginPath();
-          ctx.rect(x, y, resolution, resolution);
-          ctx.fillStyle = 'white';
-          ctx.fill();
-          ctx.strokeStyle = 'white';
-          ctx.stroke();
+    function initial2DArray(spalte, reihe) {
+        let arr = new Array(spalte);
+        for (let i = 0; i < arr.length; i++) {
+            arr[i] = new Array(reihe);
         }
-      }
+        return arr;
     }
-    let next = initial2DArray(spalte, reihe);
-    // console.log(next)
-    for (let i = 0; i < spalte; i++) {
-      for (let j = 0; j < reihe; j++) {
-        let now = grid[i][j];
-        
-        
 
-          let sum = 0;
-          let nachbar = nachbarCount(grid, i, j);
-          // console.log(nachbar);
-        
-          if (now == 0 && nachbar == 3){
-            next[i][j] = 1;
-          } else if (now == 1 && (nachbar < 2 || nachbar > 3)) {
-            next[i][j] = 0;
-          } else {
-            next[i][j] = now;
-          }
-        
-      }
+    function initialize() {
+        grid = initial2DArray(spalte, reihe);
+        for (let i = 0; i < spalte; i++) {
+            for (let j = 0; j < reihe; j++) {
+                grid[i][j] = Math.floor(Math.random() * 2);
+            }
+        }
+        // console.table(grid);
     }
-    grid = next;
-    // console.table(next)
-    
-  }
-  render();
+    initialize();
 
-  function nachbarCount(grid, x, y){
-    let sum = 0
-    for (let i = -1; i < 2; i++) {
-      for (let j = -1; j < 2; j++) {
-        // if (i === x && j === y) {
-        //   continue;
+    var myReq;
+
+    function anim() {
+        try {
+            myReq = requestAnimationFrame(anim);
+            render();
+        } catch (err) {}
+
+    }
+    anim();
+
+    let flag = true
+
+    function stop() {
+
+        if (!flag) {
+            myReq = requestAnimationFrame(anim);
+            flag = true
+        } else {
+            window.cancelAnimationFrame(myReq);
+            flag = false
+        }
+        // if (!flag) {
+        //     console.log("request")
+        //         // flag = true
+        //     myReq = requestAnimationFrame(anim);
         // }
-      let a = (x + i + spalte) % spalte;
-      let b = (y + j + reihe) % reihe;
-      // console.log(col)
-        sum += grid[a][b];
-      }
-    }
-    sum -= grid[x][y];
-    return sum;
-  
-  }
-  
-}
 
+    }
+
+    button2.addEventListener('click', stop);
+
+
+    function render() {
+        for (let i = 0; i < spalte; i++) {
+            for (let j = 0; j < reihe; j++) {
+                let x = i * resolution;
+                let y = j * resolution;
+                // console.log(grid[i][j]);
+                if (grid[i][j] == 1) {
+                    ctx.beginPath();
+                    ctx.rect(x, y, resolution, resolution);
+                    ctx.fillStyle = 'black';
+                    ctx.fill();
+                    ctx.strokeStyle = 'white';
+                    ctx.stroke();
+                } else {
+                    ctx.beginPath();
+                    ctx.rect(x, y, resolution, resolution);
+                    ctx.fillStyle = 'white';
+                    ctx.fill();
+                    ctx.strokeStyle = 'white';
+                    ctx.stroke();
+                }
+            }
+        }
+        let next = initial2DArray(spalte, reihe);
+        // console.log(next)
+        for (let i = 0; i < spalte; i++) {
+            for (let j = 0; j < reihe; j++) {
+                let now = grid[i][j];
+
+
+
+                let sum = 0;
+                let nachbar = nachbarCount(grid, i, j);
+                // console.log(nachbar);
+
+                if (now == 0 && nachbar == 3) {
+                    next[i][j] = 1;
+                } else if (now == 1 && (nachbar < 2 || nachbar > 3)) {
+                    next[i][j] = 0;
+                } else {
+                    next[i][j] = now;
+                }
+
+            }
+        }
+        grid = next;
+        // console.table(next)
+
+    }
+    render();
+
+    function nachbarCount(grid, x, y) {
+        let sum = 0
+        for (let i = -1; i < 2; i++) {
+            for (let j = -1; j < 2; j++) {
+                // if (i === x && j === y) {
+                //   continue;
+                // }
+                let a = (x + i + spalte) % spalte;
+                let b = (y + j + reihe) % reihe;
+                // console.log(col)
+                sum += grid[a][b];
+            }
+        }
+        sum -= grid[x][y];
+        return sum;
+
+    }
+
+}
